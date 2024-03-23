74a6bfed131dd792c871b525fc6a61c16eca49ed
Orkney06 <kome8728@gmail.com>
2024-03-23 23:07:43 +0900

    first commit

---
+++ src/main/kotlin/com/book/bookmanagesystem/BookManageSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.book.bookmanagesystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BookManageSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<BookManageSystemApplication>(*args)
+}
